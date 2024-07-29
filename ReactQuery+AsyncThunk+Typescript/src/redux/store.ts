69bd6e61758cdbb09eda5cb6c99b4541f8733b40
naushadraien <naushadraien5@gmail.com>
2024-07-29 19:29:59 +0545

    fix: redux type

--- ReactQuery+AsyncThunk+Typescript/src/redux/store.ts
+++ ReactQuery+AsyncThunk+Typescript/src/redux/store.ts
@@ -1,6 +1,8 @@
 import { configureStore } from "@reduxjs/toolkit";
 import clientReducer from "./clientSlice";
 import { clientApi } from "./clientApiSlice";
+import { useSelector } from "react-redux";
+import { useDispatch } from "react-redux";
 
 const store = configureStore({
   reducer: {
@@ -20,3 +22,7 @@ export default store;
 export type RootState = ReturnType<typeof store.getState>;
 // Inferred type: {posts: PostsState, comments: CommentsState, users: UsersState}
 export type AppDispatch = typeof store.dispatch;
+
+// Use throughout your app instead of plain `useDispatch` and `useSelector`
+export const useAppDispatch = useDispatch.withTypes<AppDispatch>();
+export const useAppSelector = useSelector.withTypes<RootState>();
